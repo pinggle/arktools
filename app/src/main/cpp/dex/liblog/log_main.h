dc387d94dc6501bbba2d4a577bf5d71cd37a4726
yanping <yanping@lizhi.fm>
2021-11-04 20:28:35 +0800

    添加日志打印类;

--- app/src/main/cpp/dex/liblog/log_main.h
+++ app/src/main/cpp/dex/liblog/log_main.h
@@ -17,7 +17,9 @@
 #ifndef _LIBS_LOG_LOG_MAIN_H
 #define _LIBS_LOG_LOG_MAIN_H
 
+#include <stdio.h>
 #include <android/log.h>
+#include <errno.h>
 
 
 /*
@@ -43,6 +45,69 @@
 #define ALOGV(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
 #define ALOGI(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
+
+
+#define LOG_TEXT_MAX_LENGTH        (1024)  //  单条日志大小
+#define LOG_FILE_MAX_SIZE    (1024*1024*2) //  文件最大为2MB
+
+enum {
+    LOG_LEVEL_NONE = 0,
+    LOG_LEVEL_ERR = 1,
+    LOG_LEVEL_WARNING = 2,
+    LOG_LEVEL_INFO = 3,
+    LOG_LEVEL_DEBUG = 4
+};
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+/**
+ * 初始化日志选项
+ * @param pFile
+ * @param filename
+ * @param logLevel
+ * @param printScreen
+ * @return
+ */
+bool _LogInit(const char *pFile, const char *filename, int logLevel, int printScreen);
+
+/**
+ * 写日志
+ * @param level
+ * @param strFormat
+ * @param ...
+ */
+void WriteTextLog(int level, const char *strFormat, ...);
+
+/**
+ * 向文件中写入日志
+ * @param level
+ * @param log
+ */
+void WriteTextLogBottom(int level, const char *log);
+
+/**
+ * 关闭日志库
+ */
+void _LogClose();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+//======================================================
+
+#define WRITE_LOG(s, ...) do { \
+FILE *f = fopen("/data/local/tmp/ark.log.txt", "a+"); \
+  fprintf(f, s, __VA_ARGS__); \
+  fflush(f); \
+  fclose(f); \
+} while (0)
+
 #endif
 
 #endif /* _LIBS_LOG_LOG_MAIN_H */
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,9 @@
 #include <string>
 
 #include "dex/DexDump.h"
+#include "liblog/log_main.h"
+
+static bool logEnable =false;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_dt_arktools_MainActivity_stringFromJNI(
@@ -27,4 +30,41 @@ Java_com_dt_arktools_MainActivity_dexDump(
 
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_dt_arktools_utils_LogInit(JNIEnv *env, jobject instance, jstring logFilePath,
+                                    jstring logName, jint logfileLevel, jint logScreenLevel) {
+    if (!logEnable) {
+        const char *path = env->GetStringUTFChars(logFilePath, JNI_FALSE);
+        const char *name = env->GetStringUTFChars(logName, JNI_FALSE);
+        int fileLevel = logfileLevel;
+        int screenLevel = logScreenLevel;
+        logEnable = _LogInit(path, name, fileLevel, screenLevel);
+        env->ReleaseStringUTFChars(logFilePath, path);
+        env->ReleaseStringUTFChars(logName, name);
+    }
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_dt_arktools_utils_log(JNIEnv *env, jclass type, jint _level, jstring _str) {
+    if (!logEnable) {
+        LOGE("log error! LogInit need");
+        return;
+    }
+    const char *str = env->GetStringUTFChars(_str, JNI_FALSE);
+    WriteTextLog(_level, str);
+    env->ReleaseStringUTFChars(_str, str);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_dt_arktools_utils_logClose(JNIEnv *env, jclass type) {
+    _LogClose();
+    logEnable=false;
 }
